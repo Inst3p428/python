cad863135960ba066227684a8461ba36bd60a959
Instep- Mustard <rotichallank@gmail.com>
2025-03-11 11:06:40 +0300

    Python intro

---
+++ Intro.py
@@ -0,0 +1,17 @@
+#Functions
+num=int(input("Enter an integer: "))
+def even_odd():
+    if num % 2==0:
+        return "The value ia an even number"
+    else:
+        return "The value is an odd number"
+
+print(even_odd())
+
+print(str(num))
+mynum=123
+def counter():
+    return len(str(mynum))
+
+print(counter())
+#loops
